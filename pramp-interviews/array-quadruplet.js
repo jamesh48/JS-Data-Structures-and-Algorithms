3343c5647b6ee139bf374469156d1671df8bbf3c
James Hrivnak <jameshrivnak4@gmail.com>
2022-01-03 23:03:27 -0700

    another pramp problem

---
+++ pramp-interviews/array-quadruplet.js
@@ -0,0 +1,33 @@
+function findArrayQuadruplet(arr, s) {
+  // your code goes here
+
+  arr.sort((a, b) => a - b);
+
+  let hashed = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const first = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      const second = arr[j];
+      if (first + second > s) {
+        break;
+      }
+      for (let x = j + 1; x < arr.length; x++) {
+        const third = arr[x];
+        const computed = first + second + third;
+        if (computed > s) {
+          break;
+        }
+        hashed[computed] = [first, second, third];
+        console.log(hashed);
+        if (hashed[s - computed]) {
+          return hashed[computed].concat(s - computed);
+        }
+      }
+    }
+  }
+  return [];
+}
+
+const result = findArrayQuadruplet([2, 7, 4, 0, 9, 5, 1, 3], 20);
+console.log(result);
