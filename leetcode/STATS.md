7df88cd662da6f189e83975ce43040baa4a3f012
jamesh48 <jameshrivnak4@gmail.com>
2024-03-24 14:45:04 -0600

    update

---
+++ leetcode/STATS.md
@@ -0,0 +1,4 @@
+## Leetcode Stats
+###### JS Easy Solved: 82
+###### JS Medium Solved: 6
+###### JS Hard Solved: 1
---
+++ leetcode/stats.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+(async () => {
+  const easyJSFiles = await fs.readdir('./javascript/solved/easy');
+  const mediumJSFiles = await fs.readdir('./javascript/solved/medium');
+  const hardJSFiles = await fs.readdir('./javascript/solved/hard');
+
+  const data = `## Leetcode Stats
+###### JS Easy Solved: ${easyJSFiles.length}
+###### JS Medium Solved: ${mediumJSFiles.length}
+###### JS Hard Solved: ${hardJSFiles.length}`;
+  await fs.writeFile('./STATS.md', data, 'utf-8');
+
+  console.log(data.replaceAll('#', ''));
+})();
