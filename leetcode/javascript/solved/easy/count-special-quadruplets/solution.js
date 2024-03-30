4faa5646c0b43194bbebe1d924ae75b8577b98f0
James Hrivnak <James.Hrivnak@united.com>
2024-03-29 21:52:22 -0600

    leetcode/javascript/solved/easy/count-special-quadruplets/

---
+++ leetcode/javascript/solved/easy/count-special-quadruplets/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countQuadruplets = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        for (let l = k + 1; l < nums.length; l++) {
+          if (nums[i] + nums[j] + nums[k] === nums[l]) {
+            if (i < j && j < k && k < l) {
+              count++;
+            }
+          }
+        }
+      }
+    }
+  }
+  return count;
+};
